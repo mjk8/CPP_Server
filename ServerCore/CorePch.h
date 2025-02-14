c59b0f80168ae66a60da0366eef5e15cb10ac0ff
mjk8 <mjktt8@gmail.com>
2025-02-14 14:59:42 +0900

    project setup 2

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+
+#define WIN32_LEAN_AND_MEAN
+
+
+void HelloWorld();
